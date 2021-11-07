4a8d6289e4e17eece9290c20b9c140a1e053e5c9
carloswagner1 <cwrsilva@gmail.com>
2021-11-07 18:26:03 -0300

    Atividade14

--- Atividade14/app.js
+++ Atividade14/app.js
@@ -3,37 +3,29 @@ email,
 comentario,
 resposta;
 
-function validar(){
+function enviar(){
 
     nome = document.pesquisa.elements[0].value
     email = document.getElementById("txtEmail").value
     comentario = document.getElementById("txtComentario").value
     
-    if(nome.length < 10){
+    if(nome == "" || nome.length < 10){
         alert("O campo nome tem menos de 10 caracteres")  
-              
+                      
     }else if(((email.indexOf("@") > -1) == false) || ((email.indexOf(".") > -1) == false)){
-        alert("Informe um endereço de email válido")        
+        alert("Informe um endereço de email válido")  
+
     }else if(comentario.length < 20){
         alert("O campo comentário tem menos de 20 caracteres")  
+
     }else if(document.getElementById('txtSim').checked == false && document.getElementById('txtNao').checked == false){
         alert("selecione uma resposta")
+        
     }else{
         if(document.querySelector('#txtNao').checked == true){
             alert("Que bom que você voltou a visitar esta página!")
         }else{
             alert("Volte sempre à esta página!")
         }
-        
     }
-
-    
-}
-
-function limpar(){
-    document.getElementById('txtNome').value = ''
-    document.getElementById('txtEmail').value = ''
-    document.getElementById('txtComentario').value = ''
-    document.getElementById('txtSim').checked = false
-    document.getElementById('txtNao').checked = false
 }
